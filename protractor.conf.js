de93fcc654f94e4312725bfaeb9a86c3e3c11425
Kevin Hannigan <kph1958@rit.edu>
2017-03-28 18:39:43 -0400

    remove firefox, i give up

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,10 +1,5 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['test/e2e/*.js'],
-    multiCapabilities: [{
-        browserName: 'firefox'
-    }, {
-        browserName: 'chrome'
-    }]
+    specs: ['test/e2e/*.js']
 }
