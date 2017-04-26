d535d62c7b5221507d90d4467816b71f0e8f6850
Kevin Hannigan <kph1958@rit.edu>
2017-04-26 16:04:36 -0400

    its filtering. Needs to be a teensy bit more efficient probably

--- public/js/controllers/browse-controller.js
+++ public/js/controllers/browse-controller.js
@@ -7,7 +7,11 @@ function($scope, $state, school, plans, planService, schoolService, user) {
     $scope.user = user;
     $scope.school = school;
     $scope.plans = plans;
-    $scope.filters = {};
+    $scope.filters = {
+        school: school,
+        title: '',
+        tags: []
+    };
 
     $scope.load = function(plan){
         planService.copyPublicPlan(plan);
@@ -18,4 +22,24 @@ function($scope, $state, school, plans, planService, schoolService, user) {
     .then(function(schools){
         $scope.schools = schools;
     });
-}]);
+}])
+
+.filter('planFilter', function(){
+    return function(plans, filters){
+        //School
+        plans = _.filter(plans, { school: filters.school });
+        
+        //Title
+        plans = _.filter(plans, function(plan) {
+            return plan.title.includes(filters.title);
+        });
+
+        //Tag
+        plans = _.filter(plans, function(plan) {
+            return filters.tags.length === 0 || 
+                _.intersectionBy(plan.tags, filters.tags, 'text').length > 0;
+        });
+
+        return plans;
+    };
+});
