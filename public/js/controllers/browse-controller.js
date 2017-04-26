6a1e89d963c4a47412310f611b1e88cd810c445a
Kevin Hannigan <kph1958@rit.edu>
2017-04-26 16:38:22 -0400

    making filtering more performant

--- public/js/controllers/browse-controller.js
+++ public/js/controllers/browse-controller.js
@@ -26,20 +26,25 @@ function($scope, $state, school, plans, planService, schoolService, user) {
 
 .filter('planFilter', function(){
     return function(plans, filters){
-        //School
-        plans = _.filter(plans, { school: filters.school });
-        
-        //Title
-        plans = _.filter(plans, function(plan) {
-            return plan.title.includes(filters.title);
-        });
+        return _.filter(plans, function(plan) {
+            //If schools dont match, dont include
+            if(plan.school._id !== filters.school._id) {
+                return false;
+            }
 
-        //Tag
-        plans = _.filter(plans, function(plan) {
-            return filters.tags.length === 0 || 
-                _.intersectionBy(plan.tags, filters.tags, 'text').length > 0;
-        });
+            //If plan title does not match search query, do not include
+            if(!plan.title.includes(filters.title)){
+                return false;
+            }
 
-        return plans;
+            //If there are tag filters AND none of those tags match the ones of this plan,
+            //do not include
+            if(filters.tags.length !== 0 &&
+                _.intersectionBy(plan.tags, filters.tags, 'text').length === 0) {
+                return false;
+            }
+
+            return true;
+        });
     };
 });
