a9d474ef6d66966f876a02cca7f0898a369426ca
Kevin Hannigan <kph1958@rit.edu>
2017-05-02 20:48:49 -0400

    preceding api calls with the url base

--- public/js/config/config.dev.js
+++ public/js/config/config.dev.js
@@ -2,4 +2,5 @@ angular.module('CoursePlanner')
 
 .constant('GOOGLE_CONFIG', {
     clientId: '393618634021-ifp3d7asggavq28ivpkh1hce2v7pgo8e.apps.googleusercontent.com',
-});
+})
+.constant('URL_BASE', '');
